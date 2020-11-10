e5c1b9598c011cd8bffad8e5d22d5815b124fd12
ranjith <ranjithsai446@gmail.com>
2020-11-10 15:51:51 +0530

    adding new file

--- php/db_config.php
+++ php/db_config.php
@@ -3,11 +3,8 @@
 $hostname = "localhost";
 $username = "root";
 $password = "";
-<<<<<<< HEAD
-$db_name = "";
-=======
 $db_name = "hrui_20";
->>>>>>> 3ca6b65b674d2c5062141cf02e63e1147c677843
+
 
 $con = mysqli_connect($hostname,$username,$password,$db_name);
 
