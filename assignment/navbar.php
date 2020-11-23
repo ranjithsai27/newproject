f1ce8e5f526888c2a3aa64be2d32c71c70e4f972
ranjith <ranjithsai446@gmail.com>
2020-11-23 16:30:51 +0530

    adding

--- assignment/navbar.php
+++ assignment/navbar.php
@@ -1,12 +1,20 @@
 <nav class="navbar navbar-default">
-    <div class="container-fluid">
-      <div class="navbar-header">
-        <span>BOOK <b>STORE</b> </span>
+    <div class="container">
+      <div class="table-wrapper">
+        <div class="table-title">
+          <div class="row">
+            <div class="col-sm-6">
+              <h2>Book <b>Store</b></h2>
+            </div>
+            <div class="col-sm-6">
+              <a
+                href="#addBookModal"
+                class="btn btn-success"
+                data-toggle="modal"
+                ><i class="material-icons">&#xE147;</i>
+                <span>Add New Book</span></a
+              >
+            </div>
+          </div>
         </div>
-        <ul class="nav navbar-nav">
-        <li class="active"><a href="index.php"> +Add New Book</a></li>
-        
-      </ul>
-      
-    </div>
   </nav>
